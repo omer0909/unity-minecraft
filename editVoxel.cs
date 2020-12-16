6b49bfa538e5dafb0f9e72a0b6b31a751b5e831b
Ömer Faruk Öz <71132301+omer0909@users.noreply.github.com>
2020-12-16 23:21:15 +0300

    Add files via upload

---
+++ editVoxel.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+
+public class editVoxel : MonoBehaviour
+{
+    public float renderDistance=50;
+    public float editDistance=10;
+    private List<voxelMap> mesh=new List<voxelMap>();
+    public List<Vector2Int> positions =new List<Vector2Int>();
+    public Material material;
+    private GameObject map;
+    bool add=true;
+    public static int size= 20;
+    public static int height= 100;
+    private float charachterHeight;
+    private float charachterRadius;
+
+    void Awake(){
+        CharacterController charachter=GetComponent<CharacterController>();
+        charachterRadius=charachter.radius;
+        charachterHeight=charachter.height-charachterRadius*2;
+
+        map=new GameObject("map");
+        
+        map.AddComponent<voxelMap>();
+        map.GetComponent<MeshRenderer>().material=material;
+        positions.Add(Vector2Int.zero);
+        mesh.Add(map.GetComponent<voxelMap>());
+        
+    }
+
+    bool collisionControl(Vector3Int pos){
+        bool contact=false;
+        Vector3 posCharachter=transform.position;
+
+        for(int i=0;i<2;i++){
+            Vector3 spherePos= new Vector3(posCharachter.x,(posCharachter.y+charachterHeight*0.5f)-charachterHeight*i,posCharachter.z);
+            
+            //right
+            if(spherePos.y<pos.y+0.5f+charachterRadius && spherePos.y>pos.y-0.5f-charachterRadius && spherePos.z>pos.z-0.5f-charachterRadius && spherePos.z<pos.z+0.5f+charachterRadius){
+                if(spherePos.x<pos.x+0.5f+charachterRadius && spherePos.x>pos.x-0.5f-charachterRadius){
+                    contact= true;
+                }
+            }
+            //up
+            if(spherePos.x<pos.x+0.5f+charachterRadius && spherePos.x>pos.x-0.5f-charachterRadius && spherePos.z>pos.z-0.5f-charachterRadius && spherePos.z<pos.z+0.5f+charachterRadius){
+                if(spherePos.y<pos.y+0.5f+charachterRadius && spherePos.y>pos.y-0.5f-charachterRadius){
+                    contact= true;
+                }
+            }
+            //forward
+            if(spherePos.x<pos.x+0.5f+charachterRadius && spherePos.x>pos.x-0.5f-charachterRadius && spherePos.y>pos.y-0.5f-charachterRadius && spherePos.y<pos.y+0.5f+charachterRadius){
+                if(spherePos.z<pos.z+0.5f+charachterRadius && spherePos.z>pos.z-0.5f-charachterRadius){
+                    contact= true;
+                }
+            }
+        }
+        return contact;
+    }
+
+    void mapControl(){
+        Vector2 pos=new Vector2(transform.position.x,transform.position.z);
+        Vector2Int posInt=new Vector2Int(Mathf.FloorToInt((transform.position.x/(float)size)),Mathf.FloorToInt((transform.position.z/(float)size)));
+        int distanceInt=(int)(renderDistance/size);
+
+
+        for(int x=0;x<distanceInt*2+1;x++){
+            for(int y=0;y<distanceInt*2+1;y++){
+                Vector2Int posCreated = new Vector2Int(x-distanceInt,y-distanceInt)+posInt;
+                
+                if(!positions.Contains(posCreated) && Vector2.Distance(pos-new Vector2(size*0.5f,size*0.5f),posCreated*size)<renderDistance){
+                    GameObject created=Instantiate(map,new Vector3(posCreated.x,0,posCreated.y)*size,Quaternion.identity);
+                    positions.Add(posCreated);
+                    mesh.Add(created.GetComponent<voxelMap>());
+                }
+                
+            }
+        }
+
+        for(int i=0;i<positions.Count;i++){
+            float distance=Vector2.Distance(pos-new Vector2(size*0.5f,size*0.5f),positions[i]*size);
+            if(distance<renderDistance){
+                mesh[i].gameObject.SetActive(true);
+                mesh[i].gameObject.GetComponent<MeshCollider>().enabled=distance<editDistance+size*0.5;
+
+            }else{
+                mesh[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void edit(Vector3Int pos){
+        Vector2Int index=new Vector2Int(Mathf.FloorToInt((pos.x/(float)size)),Mathf.FloorToInt((pos.z/(float)size)));
+        Vector3Int indexpos=new Vector3Int(pos.x-index.x*size,pos.y,pos.z-index.y*size);
+        
+        voxelMap meshMain=mesh[positions.IndexOf(index)];
+
+        if(add&&pos.y!=height-1){
+            if(!collisionControl(pos)){
+                meshMain.cubes[indexpos.x,indexpos.y,indexpos.z]=1;
+            }
+        }else if(pos.y!=0){
+            meshMain.cubes[indexpos.x,indexpos.y,indexpos.z]=0;
+        }
+
+
+        if(indexpos.x==size-1 && positions.Contains(index+Vector2Int.right)){
+
+            voxelMap meshlook= mesh[positions.IndexOf(index+Vector2Int.right)];
+            meshlook.leftM= meshMain;
+            meshlook.createMesh();
+            meshMain.rightM=meshlook;
+
+
+        }else if(indexpos.x==0 && positions.Contains(index+Vector2Int.left)){
+
+            voxelMap meshlook= mesh[positions.IndexOf(index+Vector2Int.left)];
+            meshlook.rightM= meshMain;
+            meshlook.createMesh();
+            meshMain.leftM=meshlook;
+
+        }
+
+        if(indexpos.z==size-1 && positions.Contains(index+Vector2Int.up)){
+
+            voxelMap meshlook= mesh[positions.IndexOf(index+Vector2Int.up)];
+            meshlook.backM= meshMain;
+            meshlook.createMesh();
+            meshMain.forwardM=meshlook;
+
+        }else if(indexpos.z==0 && positions.Contains(index+Vector2Int.down)){
+
+            voxelMap meshlook= mesh[positions.IndexOf(index+Vector2Int.down)];
+            meshlook.forwardM= meshMain;
+            meshlook.createMesh();
+            meshMain.backM=meshlook;
+
+        }
+
+        meshMain.createMesh();
+    }
+    
+    void Update()
+    {
+        mapControl();
+
+        if(Input.GetMouseButtonDown(0)||Input.GetMouseButtonDown(1)){
+            add=Input.GetMouseButtonDown(1);
+
+            RaycastHit hit;
+            Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray,out hit,editDistance)){
+                if(add){
+                    hit.normal=hit.normal*-1;
+                }
+                //right
+                if(hit.normal==Vector3.right){
+                    edit(new Vector3Int(Mathf.FloorToInt(hit.point.x),Mathf.RoundToInt(hit.point.y),Mathf.RoundToInt(hit.point.z)));
+                }
+                //left
+                if(hit.normal==Vector3.left){
+                    edit(new Vector3Int(Mathf.CeilToInt(hit.point.x),Mathf.RoundToInt(hit.point.y),Mathf.RoundToInt(hit.point.z)));
+                }
+                //up
+                if(hit.normal==Vector3.up){
+                    edit(new Vector3Int(Mathf.RoundToInt(hit.point.x),Mathf.FloorToInt(hit.point.y),Mathf.RoundToInt(hit.point.z)));
+                }
+                //down
+                if(hit.normal==Vector3.down){
+                    edit(new Vector3Int(Mathf.RoundToInt(hit.point.x),Mathf.CeilToInt(hit.point.y),Mathf.RoundToInt(hit.point.z)));
+                }
+                //forward
+                if(hit.normal==Vector3.forward){
+                    edit(new Vector3Int(Mathf.RoundToInt(hit.point.x),Mathf.RoundToInt(hit.point.y),Mathf.FloorToInt(hit.point.z)));
+                }
+                //back
+                if(hit.normal==Vector3.back){
+                    edit(new Vector3Int(Mathf.RoundToInt(hit.point.x),Mathf.RoundToInt(hit.point.y),Mathf.CeilToInt(hit.point.z)));
+                }
+            }
+        }
+    }
+}
---
+++ voxelMap.cs
@@ -0,0 +1,252 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+
+public class voxelMap : MonoBehaviour
+{
+    public Vector3Int size;
+    public int[,,] cubes;
+    private MeshFilter meshFilter;
+    private MeshCollider meshCollider;
+    private List<int> tris=new List<int>();
+    private List<Vector3> normals=new List<Vector3>();
+    private List<Vector2> uvs=new List<Vector2>();
+    public voxelMap rightM,leftM,forwardM,backM;
+    private int index=0;
+    private float bigDetailMultiplay=0.005f;
+    private float detailMultiplay=0.025f;
+    private float detailHeightMultiplay=40;
+    private int mapHeight=10;
+
+
+    void Awake()
+    {
+        size=new Vector3Int(editVoxel.size,editVoxel.height,editVoxel.size);
+
+        cubes=new int[size.x,size.y,size.z];
+
+        meshFilter = GetComponent<MeshFilter>();
+
+        meshCollider = GetComponent<MeshCollider>();
+
+        for (int x=0;x<size.x;x++){
+            for (int y=0;y<size.y;y++){
+                for (int z=0;z<size.z;z++){
+
+                    float bigDetail=Mathf.PerlinNoise((x+transform.position.x-99999)*bigDetailMultiplay,(z+transform.position.z-99999)*bigDetailMultiplay);
+
+                    int maxHeight=Mathf.FloorToInt(Mathf.PerlinNoise((x+transform.position.x-99999)*detailMultiplay,(z+transform.position.z-99999)*detailMultiplay)*detailHeightMultiplay*bigDetail);
+
+                    if(y<=maxHeight+mapHeight){
+                        cubes[x,y,z]=1;
+                    }
+                    
+
+                }
+            }
+        }
+        createMesh();
+    }
+    
+    void createFace(Vector3 normal){
+        int[] trisA={
+            index+0,index+1,index+2,
+            index+2,index+1,index+3
+        };
+        Vector3[] normalsA={
+            normal,
+            normal,
+            normal,
+            normal
+        };
+        Vector2[] uvsA={
+            new Vector2(1,0),
+            new Vector2(1,1),
+            new Vector2(0,0),
+            new Vector2(0,1)
+        };
+                            
+        tris.AddRange(trisA);
+        normals.AddRange(normalsA);
+        uvs.AddRange(uvsA);
+        index+=4;
+    }
+    public void createMesh(){
+        Mesh mesh=new Mesh();
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        
+        List<Vector3> vertices=new List<Vector3>();
+
+        for (int x=0;x<size.x;x++){
+            for (int y=0;y<size.y;y++){
+                for (int z=0;z<size.z;z++){
+                    if(cubes[x,y,z]!=0){
+                        
+                        //right
+                        bool right=false;
+                        if(x!=size.x-1){
+                            if(cubes[x+1,y,z]==0){
+                                right=true;
+                            }
+                        }else if(rightM!=null){
+                                right=rightM.cubes[0,y,z]==0;
+
+                        }else{
+                            float bigDetail=Mathf.PerlinNoise((x+1+transform.position.x-99999)*bigDetailMultiplay,(z+transform.position.z-99999)*bigDetailMultiplay);
+                            int maxHeight=Mathf.FloorToInt(Mathf.PerlinNoise((x+1+transform.position.x-99999)*detailMultiplay,(z+transform.position.z-99999)*detailMultiplay)*detailHeightMultiplay*bigDetail);
+                            right=y>maxHeight+mapHeight;
+
+                        }
+                        
+
+                        //left
+                        bool left=false;
+                        if(x!=0){
+                            if(cubes[x-1,y,z]==0){
+                                left=true;
+                            }
+                        }else if(leftM!=null){
+                            left=leftM.cubes[size.x-1,y,z]==0;
+
+                        }else{
+                            float bigDetail=Mathf.PerlinNoise((x-1+transform.position.x-99999)*bigDetailMultiplay,(z+transform.position.z-99999)*bigDetailMultiplay);
+                            int maxHeight=Mathf.FloorToInt(Mathf.PerlinNoise((x-1+transform.position.x-99999)*detailMultiplay,(z+transform.position.z-99999)*detailMultiplay)*detailHeightMultiplay*bigDetail);
+                            left=y>maxHeight+mapHeight;
+                        }
+
+                        //up
+                        bool up=false;
+                        if(y!=size.y-1){
+                            if(cubes[x,y+1,z]==0){
+                                up=true;
+                            }
+                        }else{
+                            up=true;
+                        }
+                        
+                        //down
+                        bool down=false;
+                        if(y!=0){
+                            if(cubes[x,y-1,z]==0){
+                                down=true;
+                            }
+                        }
+
+                        //forward
+                        bool forward=false;
+                        if(z!=size.z-1){
+                            if(cubes[x,y,z+1]==0){
+                                forward=true;
+                            }
+                        }else if(forwardM!=null){
+                            forward=forwardM.cubes[x,y,0]==0;
+
+                        }else{
+                            float bigDetail=Mathf.PerlinNoise((x+transform.position.x-99999)*bigDetailMultiplay,(z+1+transform.position.z-99999)*bigDetailMultiplay);
+                            int maxHeight=Mathf.FloorToInt(Mathf.PerlinNoise((x+transform.position.x-99999)*detailMultiplay,(z+1+transform.position.z-99999)*detailMultiplay)*detailHeightMultiplay*bigDetail);
+                            forward=y>maxHeight+mapHeight;
+                        }
+
+                        //back
+                        bool back=false;
+                        if(z!=0){
+                            if(cubes[x,y,z-1]==0){
+                                back=true;
+                            }
+                        }else if(backM!=null){
+                            back=backM.cubes[x,y,size.z-1]==0;
+
+                        }else{
+                            float bigDetail=Mathf.PerlinNoise((x+transform.position.x-99999)*bigDetailMultiplay,(z-1+transform.position.z-99999)*bigDetailMultiplay);
+                            int maxHeight=Mathf.FloorToInt(Mathf.PerlinNoise((x+transform.position.x-99999)*detailMultiplay,(z-1+transform.position.z-99999)*detailMultiplay)*detailHeightMultiplay*bigDetail);
+                            back=y>maxHeight+mapHeight;
+                        }
+
+
+                        if(right){
+                            Vector3[] verticesA={
+                                new Vector3(x+0.5f,y+0.5f,z-0.5f),
+                                new Vector3(x+0.5f,y+0.5f,z+0.5f),
+                                new Vector3(x+0.5f,y-0.5f,z-0.5f),
+                                new Vector3(x+0.5f,y-0.5f,z+0.5f)
+                            };
+                            vertices.AddRange(verticesA);
+                            createFace(Vector3.right);
+                        }
+                        
+                        if(left){
+                            Vector3[] verticesA={
+                                new Vector3(x-0.5f,y+0.5f,z+0.5f),
+                                new Vector3(x-0.5f,y+0.5f,z-0.5f),
+                                new Vector3(x-0.5f,y-0.5f,z+0.5f),
+                                new Vector3(x-0.5f,y-0.5f,z-0.5f)
+                            };
+                            vertices.AddRange(verticesA);
+                            createFace(Vector3.left);
+                        }
+                        
+                        if(up){
+                            Vector3[] verticesA={
+                                new Vector3(x+0.5f,y+0.5f,z+0.5f),
+                                new Vector3(x+0.5f,y+0.5f,z-0.5f),
+                                new Vector3(x-0.5f,y+0.5f,z+0.5f),
+                                new Vector3(x-0.5f,y+0.5f,z-0.5f)
+                            };
+                            vertices.AddRange(verticesA);
+                            createFace(Vector3.up);
+                        }
+                        
+                        if(down){
+                                Vector3[] verticesA={
+                                new Vector3(x+0.5f,y-0.5f,z-0.5f),
+                                new Vector3(x+0.5f,y-0.5f,z+0.5f),
+                                new Vector3(x-0.5f,y-0.5f,z-0.5f),
+                                new Vector3(x-0.5f,y-0.5f,z+0.5f)
+                            };
+                            vertices.AddRange(verticesA);
+                            createFace(Vector3.down);
+                        }
+                        
+                        if(forward){
+                            Vector3[] verticesA={
+                                new Vector3(x+0.5f,y-0.5f,z+0.5f),
+                                new Vector3(x+0.5f,y+0.5f,z+0.5f),
+                                new Vector3(x-0.5f,y-0.5f,z+0.5f),
+                                new Vector3(x-0.5f,y+0.5f,z+0.5f)
+                            };
+                            vertices.AddRange(verticesA);
+                            createFace(Vector3.forward);
+                        }
+                        
+                        
+                        if(back){
+                            Vector3[] verticesA={
+                                new Vector3(x+0.5f,y+0.5f,z-0.5f),
+                                new Vector3(x+0.5f,y-0.5f,z-0.5f),
+                                new Vector3(x-0.5f,y+0.5f,z-0.5f),
+                                new Vector3(x-0.5f,y-0.5f,z-0.5f)
+                            };
+                            vertices.AddRange(verticesA);
+                            createFace(Vector3.forward);
+                        }
+                    }
+                }
+            }
+        }
+        
+        mesh.vertices=vertices.ToArray();
+        mesh.normals=normals.ToArray();
+        mesh.uv=uvs.ToArray();
+        mesh.triangles=tris.ToArray();
+        meshFilter.mesh=mesh;
+        meshCollider.sharedMesh=mesh;
+
+        index=0;
+        tris.Clear();
+        normals.Clear();
+        uvs.Clear();
+    }
+}
